dde2b17e3ca2093b6b970b9c6797d66b9d27e120
Aleksey Batuev <abatuev@cloudlinux.com>
2022-05-16 10:12:48 +0500

    fixed inserting

--- addrent.php
+++ addrent.php
@@ -10,13 +10,13 @@
     <h1 style="text-align: center;">Арендовать</h1>
     <form method="get" action="insertrent.php">
         <div class="mb-3">
-            <select name="user_id" class="form-select">
+            <select name="user_id" class="form-select" >
                 <?php
                     $sql = "SELECT * FROM user WHERE user_id=".$_SESSION['user_id'];
                     $result = $conn->query($sql);
                     while($row = $result->fetch())
                     {
-                        echo '<option value="'.$row['user_id'].'">'.$row['first_name']. " ".$row['second_name'].'</option>';
+                        echo '<option readonly value="'.$row['user_id'].'">'.$row['first_name']. " ".$row['second_name'].'</option>';
                     }
                 ?>
             </select>
@@ -24,7 +24,7 @@
         <div class="mb-3">
             <select name="property_id" class="form-select">
                 <?php
-                    $sql = "SELECT * FROM property WHERE".$_GET['property_id'];
+                    $sql = "SELECT * FROM property WHERE property_id=".$_GET['property_id'];
                     $result = $conn->query($sql);
                     while($row = $result->fetch())
                     {
@@ -41,7 +41,7 @@
         </div>
         <div class="mb-3">
             <?php
-                echo '<input type="number" name="price" placeholder="price" class="form-control" value="'.$_GET['price'].'">'
+                echo '<input type="number" name="price" placeholder="price" class="form-control" value="'.$_GET['price'].'" readonly>'
             ?>
         </div>
         <div class="mb-3" style="display: flex; justify-content: start">
